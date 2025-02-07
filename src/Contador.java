1260d3c1d5c971b8d4c63dd2e363b9c0c32a54ab
FrancineDelanni02 <delannifrancine@gmail.com>
2025-02-07 17:01:27 -0300

    Finalizando

--- src/Contador.java
+++ src/Contador.java
@@ -2,28 +2,28 @@ package src;
 import java.util.Scanner;
 
 public class Contador {
-//    public static void main(String[] args) {
-//        Scanner terminal = new Scanner(System.in);
-//        System.out.println("Digite o primeiro parâmetro");
-//        int parametroUm = terminal.??;
-//        System.out.println("Digite o segundo parâmetro");
-//        int parametroDois = terminal.??;
-//
-//        try {
-//            //chamando o método contendo a lógica de contagem
-//            contar(parametroUm, parametroDois);
-//
-//        }catch (? exception) {
-//            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-//        }
-//
-//    }
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    }
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
         if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException();
         }
 
         int contagem = parametroDois - parametroUm;
-        //realizar o for para imprimir os números com base na variável contagem
+        for(int i=0;i<contagem;i++){
+            System.out.println("Imprimindo o número " + (i+1));
+        }
     }
 }
--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,5 +1,7 @@
 package src;
 
 public class ParametrosInvalidosException extends Exception {
-
+    public ParametrosInvalidosException(){
+        super();
+    }
 }
